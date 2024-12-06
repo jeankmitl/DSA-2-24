83d67c1ebd60fc35b7d04c012518a4e40e477534
jeankmitl <67070279@kmitl.ac.th>
2024-12-06 13:14:44 +0700

    Update 05-Student_Class.py

--- Week1/05-Student_Class.py
+++ Week1/05-Student_Class.py
@@ -2,10 +2,28 @@
 class Data:
     def __init__(student, name, sex, age, studentid, gpa):
         student.name = name
-        student.sex = sex
+        if sex in "Male":
+            student.sex = 'Mr'
+        else:
+            student.sex = 'Miss'
         student.age = age
         student.studentid = studentid
         student.gpa = gpa
 
     def __str__(student):
-        return f"{student.sex} {student.name} ({student.age}) ID: {student.studentid} GPA {student.gpa:.2f}"
+        return f"{student.sex} {student.name} ({student.age}) ID: {student.studentid} GPA {float(student.gpa):.2f}"
+
+PERSONLIST = []
+for _ in range(3):
+    PERSONLIST.append(Data(input(), input(), input(),input(), input()))
+
+STUID = input()
+
+FOUND = False
+for i in PERSONLIST:
+    if i.studentid == STUID:
+        print(i)
+        FOUND = True
+
+if not FOUND:
+    print('Student not found')
