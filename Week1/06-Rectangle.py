a52e15b69f36f7f99ccf557f1254532041132951
jeankmitl <67070279@kmitl.ac.th>
2024-12-07 14:17:39 +0700

    added moire

---
+++ Week1/06-Rectangle.py
@@ -0,0 +1,21 @@
+'''rectangle'''
+class Rectangle:
+  '''rectangle'''
+  def __init__(self, height, width):
+    self.height = height
+    self.width = width
+
+  def calculate_area(self):
+    return self.height * self.width
+
+  def calculate_perimeter(self):
+    return (self.height + self.width) * 2
+  
+RECTANGLE = Rectangle(float(input()), float(input()))
+
+CONDITION = input()
+if CONDITION == "area":
+    RESULT = RECTANGLE.calculate_area()
+else:
+    RESULT = RECTANGLE.calculate_perimeter()
+print(f"{RESULT:.2f}")
